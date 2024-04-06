a43ac0d6247b9757e5f418e395180179e42138c5
Daniel Sanchez <springnacho@gmail.com>
2024-04-05 22:48:22 -0500

    Create bd_producto.sql

---
+++ bd_producto.sql
@@ -0,0 +1,67 @@
+SELECT * FROM bd_producto.tb_producto;
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/127625_7.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Xbox Series X 1TB', 'Presentamos Xbox Series X, la Xbox más rápida y potente que nunca. Juega miles de títulos de las cuatro generaciones de consolas, todos los juegos se ven y juegan mejor en Xbox Series X.', 'Microsoft', 'Consola', 2799.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/p/s/ps5_d_sa_rndr_lt_prod_rgb_la_240105-_1_.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'PlayStation 5 1TB SSD', 'Disfruta de tiempos de carga superveloces con un SSD de velocidad ultrarrápida, una experiencia más inmersiva gracias a la compatibilidad con respuesta háptica, gatillos adaptativos y audio 3D.', 'Sony', 'Consola', 2699.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/128157-2.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Nintendo Switch OLED 64GB', 'La nueva consola cuenta con una vibrante pantalla OLED de 7 pulgadas, un soporte ajustable y amplio, una base con puerto LAN para conexión por cable, almacenamiento de 64 GB y audio mejorado.', 'Nintendo', 'Consola', 1499.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/124006.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Audífonos G733 con micrófono inalámbricos', 'Audífonos inalámbricos con micrófono para juegos diseñados para desempeño y confort. Equipados con todo el sonido envolvente, los filtros de voz y la iluminación avanzada que necesitas.', 'Logitech', 'Accesorios', 594.90);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/_/1_394.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Mouse Logitech G903 Lightspeed', 'El sensor HERO 25K cuenta con un seguimiento submicrónico. Diseño ambidiestro. Desplazamiento superrápido. Compatible con POWERPLAY para tener carga inalámbrica continua.', 'Logitech', 'Accesorio', 449.10);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/3/130975-2.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Silla Gamer con Masajeador Lumbar Kuzler RIK-101B', 'La silla gamer Kuzler RIK-101B es un producto diseñado específicamente para brindar comodidad y soporte a los usuarios durante largas sesiones de juego o trabajo en frente de un ordenador.', 'Kuzler', 'Accesorio', 399.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/127681___1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'God of War Ragnarok PS4', 'God of War Ragnarök es un videojuego de acción y aventura hack and slash en tercera persona desarrollado por Santa Monica Studio y publicado por Sony Interactive Entertainment.', 'Sony', 'Videojuego', 289.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/127258.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Grand Theft Auto V PS5', 'Es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio escocés Rockstar North y distribuido por Rockstar Games. Estrenado el 17 de setiembre de 2013', 'Rockstar Games', 'Videojuego', 219.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/3/130854_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Logitech G815 RGB Lightsync Negro', 'Lo último en tecnología de juegos. Diseño ultrafino. RGB LIGHTSYNC y teclas G específicas. Diseñado con interruptores mecánicos GL de perfil bajo en versiones de click perceptible, táctil y lineal.', 'Logitech', 'Accesorio', 772.60);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/4/5/45496599782-1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Red Dead Redemption Nintendo Switch', 'Red Dead Redemption es un videojuego de acción-aventura de mundo abierto no lineal desarrollado por Rockstar San Diego y publicado por Rockstar Games.', 'Rockstar Games', 'Videojuego', 219.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/s/h/shifter-gallery-1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Logitech Driving Force Shifter', 'Para volantes y pedales G923, G29 y G920. Con un robusto eje de acero, una caja de cambios con recorrido corto de seis velocidades, y funda y empuñadura de cuero.', 'Logitech', 'Accesorios', 349.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/0/109252_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Joy Con (Izq/Der) Nintendo Switch', 'Mandos Joy-Con, derecho e izquierdo, con dos correas de los mandos Joy-Con. Según el juego, se pueden usar varios pares de Joy-Con para jugar en una consola Nintendo Switch con más jugadores.', 'Nintendo', 'Accesorios', 389.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/1/115361_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Super Mario Party', 'Super Mario Party es un videojuego de fiesta desarrollado por Nd Cube y publicado por Nintendo. Fecha de estreno inicial: 5 de octubre de 2018', 'Nintendo', 'Videojuego', 259.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/125238.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Mortal Kombat 11 Ultimate Edition PS5', 'Mortal Kombat 11 es un videojuego de lucha desarrollado por NetherRealm Studios y publicado por Warner Bros. Interactive Entertainment.', 'Warner Bros. Interactive Entertainment', 'Videojuego', 319.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/3/130321_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Monitor Gamer LG UltraGear FHD 23.8"', 'Pantalla IPS de 23.8 pulgadas FHD (1920x1080), 1ms (GtG at Faster), Frecuencia de actualización de 144Hz, sRGB 99% (CIE1931) y HDR10. Compatible con FreeSync Premium', 'LG', 'Accesorio', 849.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/p/h/phs-s110at_01.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Primus Ahsoka PHS-S110AT', 'Primus Gaming - PHS-S110AT - Headset - Para Computer / Para Game console - Wired - Ahsoka Tano Arcus110T', 'Primus Gaming', 'Accesorio', 153.20);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/3/d/3d_front.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Call of Duty: Modern Warfare 3 PS5', 'Call of Duty: Modern Warfare 3 es un videojuego de disparos en primera persona desarrollado por Infinity Ward y Sledgehammer Games.', 'Infinity Ward, Sledgehammer Games', 'Videojuego', 319.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/2/125204_1_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Mando Sony DualShock 4 PS4', 'Toma el control de una nueva generación de videojuegos con un mando inalámbrico DUALSHOCK 4 rediseñado que pone en tus manos la mayor precisión en el juego.', 'Sony', 'Accesorio', 329.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/r/c/rc71l-nh014w.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Asus ROG Ally Z1 Extreme 512GB SSD', 'La ROG Ally es una verdadera máquina gaming con Windows 11. Con los nuevos procesadores AMD Ryzen™ serie Z1, la ROG Ally es una máquina gaming portátil increíblemente potente con gráficos RDNA™3.', 'Asus', 'Consola', 3099.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/3/130772_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'Cámara Web Razer Kiyo FHD Negro', 'Resolución máxima de video: 1920px x 1080px. Imagen con resolución de 4Mpx. Interfaz: USB 2.0. Funciona con Windows 7. Adecuada para notebook. Trae auto-foco.', 'Razer', 'Accesorio', 259.00);
+
+insert into tb_producto (imagenurl, nombre, descripcion, marca, categoria, precio)
+values ('https://hiraoka.com.pe/media/catalog/product/1/3/130307_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=700&canvas=700:560', 'The Legend of Zelda: Breath of the Wild - Nintendo Switch', 'Entra en un mundo de descubrimientos, exploración y aventura en The Legend of Zelda: Breath of the Wild, un nuevo juego de la aclamada serie que rompe con las convenciones.', 'Nintendo', 'Videojuego', 199.00);
+
+select * from tb_producto;
+
